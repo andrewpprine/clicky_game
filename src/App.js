777574cea80617c9523f4a1a4f6b39e71213bb29
Andrew Prine <andrewpprine@gmail.com>
2018-11-14 12:07:26 -0800

    food app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+import cards from "./components/cards.json";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    cards,
+    clickedCardsIds: [],
+    score: 0,
+    goal: 8,
+    status: ""
+  };
+
+  shuffleScoreCard = id => {
+    let clickedCardsIds = this.state.clickedCardsIds;
+
+    if (clickedCardsIds.includes(id)) {
+      this.setState({
+        clickedCardsIds: [],
+        score: 0,
+        status: "You lost, try again"
+      });
+      return;
+    } else {
+      clickedCardsIds.push(id);
+
+      if (clickedCardsIds.length === 8) {
+        this.setState({ Score: 8, status: "You Won!!!!", clickedCardsIds: [] });
+        console.log("You Win");
+        return;
+      }
+
+      this.setState({
+        cards,
+        clickedCardsIds,
+        score: clickedCardsIds.length,
+        status: " "
+      });
+
+      for (let i = cards.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+      }
+    }
+  };
+
+  // Map over this.state.cards and render a Card component for each card object
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Clicky Memory Game</h1>
+          <p className="App-intro">
+            Click a food, any food. The pictures will shuffle, then your job is
+            to click a DIFFERENT food. Get a point for each different food, but
+            if you click any one a second time then you'll reset to 0.
+          </p>
         </header>
-        <p className="App-intro">
-          Click a food, any food. Then click another DIFFERENT food.
-        </p>
-        <p className="App-intro">
-          With each click, the pictures will shuffle.
-        </p>
-        <p className="App-intro">
-          Get a point for each time you click a different food, but if you click one a second time then you'll reset to 0.
-        </p>
-        <p className="App-intro">
-          <b>Happy clicking, and try not to get too hungry.</b>
-        </p>
-      </div>
-      <div id="pictures">
-
+        <Score total={this.state.score} goal={12} status={this.state.status} />
+        <Wrapper>
+          {this.state.cards.map(card => (
+            <Card
+              shuffleScoreCard={this.shuffleScoreCard}
+              id={card.id}
+              key={card.id}
+              image={card.image}
+            />
+          ))}
+        </Wrapper>
+        <footer>
+          <p>Thanks.</p>
+        </footer>
       </div>
     );
   }
 }
-
 export default App;
